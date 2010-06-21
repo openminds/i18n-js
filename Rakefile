1a746fde6f3fd088a100f56be6d0ed63e72d1bcc
Jan De Poorter <jan@defv.be>
2010-06-21 15:43:58 +0200

    Make this a Rails 3 compatible gem

--- Rakefile
+++ Rakefile
@@ -1,6 +1,5 @@
 require 'rake'
 require 'rake/testtask'
-require 'hanna/rdoctask'
 
 desc 'Default: run unit tests.'
 task :default => :test
@@ -13,11 +12,35 @@ Rake::TestTask.new(:test) do |t|
   t.verbose = true
 end
 
-desc 'Generate documentation for the i18n-js plugin.'
-Rake::RDocTask.new(:rdoc) do |rdoc|
-  rdoc.rdoc_dir = 'doc'
-  rdoc.title    = 'I18n for JavaScript'
-  rdoc.options << '--line-numbers' << '--inline-source'
-  rdoc.rdoc_files.include('README.rdoc')
-  rdoc.rdoc_files.include('lib/**/*.rb')
+begin
+  require 'hanna/rdoctask'
+  
+  desc 'Generate documentation for the i18n-js plugin.'
+  Rake::RDocTask.new(:rdoc) do |rdoc|
+    rdoc.rdoc_dir = 'doc'
+    rdoc.title    = 'I18n for JavaScript'
+    rdoc.options << '--line-numbers' << '--inline-source'
+    rdoc.rdoc_files.include('README.rdoc')
+    rdoc.rdoc_files.include('lib/**/*.rb')
+  end
+rescue LoadError
+  puts "hanna/rdoctask not available."
+end
+
+begin
+  require 'jeweler'
+  
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "i18n-js"
+    gemspec.summary = "It's a small library (5.2KB or 1.76KB when gzipped) to provide the Rails I18n translations on the Javascript."
+    gemspec.description = "It's a small library (5.2KB or 1.76KB when gzipped) to provide the Rails I18n translations on the Javascript."
+    gemspec.email = "fnando.vieira@gmail.com"
+    gemspec.homepage = "http://github.com/fnando/i18n-js"
+    gemspec.authors = ["Nando Vieira"]
+    gemspec.add_development_dependency "activesupport", ">= 2.3.4"
+  end
+
+  Jeweler::GemcutterTasks.new
+rescue LoadError
+  puts "Jeweler not available. Install it with: sudo gem install jeweler"
 end
--- lib/i18n-js.rb
+++ lib/i18n-js.rb
@@ -1,15 +1,33 @@
 module SimplesIdeias
   module I18n
+    if Rails.const_defined?(:Railtie)
+      class Railtie < Rails::Railtie
+        rake_tasks do
+          load 'tasks/i18n-js_tasks.rake'
+        end
+        
+        config.i18n_js = ActiveSupport::OrderedOptions.new
+      end
+    end
+    
     extend self
 
     # deep_merge by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
     MERGER = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &MERGER) : v2 }
 
     # Set configuration file path
-    CONFIG_FILE = Rails.root.join("config/i18n-js.yml")
+    CONFIG_FILE = "config/i18n-js.yml"
 
     # Set i18n.js output path
-    JAVASCRIPT_FILE = Rails.root.join("public/javascripts/i18n.js")
+    JAVASCRIPT_FILE = "public/javascripts/i18n.js"
+
+    def config_file
+      @config_file ||= Rails.root.join(CONFIG_FILE)
+    end
+    
+    def javascript_file
+      @javascript_file ||= Rails.root.join(JAVASCRIPT_FILE)
+    end
 
     # Export translations to JavaScript, considering settings
     # from configuration file
@@ -33,26 +51,26 @@ module SimplesIdeias
     # Load configuration file for partial exporting and
     # custom output directory
     def config
-      HashWithIndifferentAccess.new YAML.load_file(CONFIG_FILE)
+      HashWithIndifferentAccess.new YAML.load_file(config_file)
     end
 
     # Check if configuration file exist
     def config?
-      File.file? CONFIG_FILE
+      File.file? config_file
     end
 
     # Copy configuration and JavaScript library files to
     # <tt>SimplesIdeias::I18n::CONFIG_FILE</tt> and <tt>public/i18n.js</tt>.
     def setup!
-      FileUtils.cp File.dirname(__FILE__) + "/i18n.js", JAVASCRIPT_FILE
-      FileUtils.cp(File.dirname(__FILE__) + "/i18n-js.yml", CONFIG_FILE) unless config?
+      FileUtils.cp File.dirname(__FILE__) + "/i18n.js", javascript_file
+      FileUtils.cp(File.dirname(__FILE__) + "/i18n-js.yml", config_file) unless config?
     end
 
     # Retrieve an updated JavaScript library from Github.
     def update!
       require "open-uri"
       contents = open("http://github.com/fnando/i18n-js/raw/master/lib/i18n.js").read
-      File.open(JAVASCRIPT_FILE, "w+") {|f| f << contents}
+      File.open(javascript_file, "w+") {|f| f << contents}
     end
 
     # Convert translations to JSON string and save file.
